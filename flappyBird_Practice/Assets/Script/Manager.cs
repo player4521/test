3364763ba2db172b05020686796ae405f324d101
player0 <nanuri@mac.com>
2020-06-03 18:54:02 +0900

    first commit

---
+++ flappyBird_Practice/Assets/Script/Manager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    private static Manager _instance = null;
+    public static Manager Instance { get { return _instance; } }
+
+    [SerializeField]
+    private Bird _bird = null;
+    [SerializeField]
+    private Ground _ground = null;
+    [SerializeField]
+    private Pipe _pipe = null;
+
+    void Awake()
+    {
+        _instance = this;
+    }
+
+    void Update()
+    {
+
+    }
+}
